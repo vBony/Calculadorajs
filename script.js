450806ebe5f926310f025900948d86085e840fc4
Vinicius Ferreira <i1ddvdscr666n7z@outlook.com>
2019-07-19 17:47:36 -0300

    Foi criado o script que faz as operações da calculadora

---
+++ script.js
@@ -0,0 +1,40 @@
+// var num1 = document.getElementById("num1").value;
+// var num2 = document.getElementById("num2").value;
+// var result = document.getElementById("resultado").innerHTML;
+
+function botaoAdicao(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = document.getElementById("resultado").innerHTML;
+
+    result = parseInt(num1) + parseInt(num2);
+    document.getElementById("resultado").innerHTML = result;
+}
+
+function botaoSub(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = document.getElementById("resultado").innerHTML;
+
+    result = parseInt(num1) - parseInt(num2);
+    document.getElementById("resultado").innerHTML = result;
+}
+
+function botaoDiv(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = document.getElementById("resultado").innerHTML;
+
+    result = parseInt(num1) / parseInt(num2);
+    document.getElementById("resultado").innerHTML = result;
+
+}
+
+function botaoMult(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = document.getElementById("resultado").innerHTML;
+
+    result = parseInt(num1) * parseInt(num2);
+    document.getElementById("resultado").innerHTML = result;
+}
